850a7cb14af2add3c2cf57bf7fc115bd8ef970b4
ashok <ashokau23@gmail.com>
2023-11-12 12:46:34 +0530

    js file added

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 //basic js for a website behaviour.
+
+const close = document.querySelector(".close");
+const open = document.querySelector(".ham");
+const menu = document.querySelector(".menu");
+close.addEventListener("click", () => {
+  menu.style.visibility = "hidden";
+});
+open.addEventListener("click", () => {
+  menu.style.visibility = "visible";
+});S
