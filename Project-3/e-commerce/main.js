c9e18eabfa0d1beef405d56fdbc82a7f7bef940e
projectsbyRA0027 <ashoktheni2712@gmail.com>
2024-06-15 18:23:55 +0530

    html,css portion complted.

--- Project-3/e-commerce/main.js
+++ Project-3/e-commerce/main.js
@@ -1,1 +1,17 @@
-alert("welcome to footzone.in")
+'use strict';
+
+// navbar toggle
+
+const overlay = document.querySelector("[data-overlay]");
+const navOpenBtn = document.querySelector("[data-nav-open-btn]");
+const navbar = document.querySelector("[data-navbar]");
+const navCloseBtn = document.querySelector("[data-nav-close-btn]");
+
+const navElms = [overlay,navCloseBtn,navCloseBtn];
+
+for(let i=0;i<navElms.length;i++){
+    navElms[i].addEventListener("click",function(){
+        navbar.classList.toggle("active");
+        overlay.classList.toggle("active");
+    });
+}
